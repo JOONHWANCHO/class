573b2b452e0bee6609e9dbcc1ddc4a26bdbdb9c9
JOONHWANCHO <joonhwanncho@gmail.com>
2022-06-16 20:56:02 +0700

    0616practice

---
+++ CarExample.java
@@ -0,0 +1,30 @@
+package test;
+
+public class CarExample {
+
+
+	
+	
+	public static void main(String[] args) {
+
+		//객체생성
+		Car myCar = new Car();
+		
+		//필드
+		System.out.println("제작회사:" + myCar.company);
+		System.out.println("모델명:" + myCar.model);
+		System.out.println("색깔:" + myCar.color);
+		System.out.println("최고속도:" + myCar.maxSpeed);
+		System.out.println("현재속도:" + myCar.speed);
+		
+		//필드 변경
+		myCar.speed = 60;
+		System.out.println("수정된 속도:" + myCar.speed);
+		
+		//생성자
+		
+		//매소
+
+	}
+
+}
---
+++ IfElseExample.java
@@ -0,0 +1,16 @@
+package test;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		
+		int score = 89;
+		
+		if(score >= 90) {
+			
+			System.out.println("점수가 90보다 큽니다");
+		}
+		
+	}
+
+}
